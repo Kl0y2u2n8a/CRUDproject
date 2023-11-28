7dd05216008dcfbb85eb8c43e6842dbd20b78286
Kl0y2u2n8a <kevinliang228@gmail.com>
2023-11-28 16:21:02 +0900

    basic set up

--- index.tw.php
+++ index.tw.php
@@ -6,12 +6,12 @@
                 <ul class="navbar-nav ms-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle text-white-50"role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Language
+                            語言
                         </a>
                         <ul class="dropdown-menu dropdown-menu-dark  text-white-50">
-                            <li><a class="dropdown-item" href="index.jp.php"><img src="img/japan.png" width="12" height="12"> Japanese</a></li>
-                            <li><a class="dropdown-item" href="index.php"><img src="img/usa.png" width="12" height="12"> English</a></li>
-                            <li><a class="dropdown-item" href="index.tw.php"><img src="img/taiwan.png" width="12" height="12"> Traditional Chinese</a></li>
+                            <li><a class="dropdown-item" href="index.jp.php"><img src="img/japan.png" width="12" height="12"> 日文</a></li>
+                            <li><a class="dropdown-item" href="index.php"><img src="img/usa.png" width="12" height="12"> 英文</a></li>
+                            <li><a class="dropdown-item" href="index.tw.php"><img src="img/taiwan.png" width="12" height="12"> 繁體中文</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -22,44 +22,44 @@
 
     <div class="container">
         <div class="text-center text-white mb-4" style="margin-top: 5%;">
-            <h3>Add New User</h3>
-            <p class="text-white-50">Please complete the form to create new user</p>
+            <h3>新增新用戶</h3>
+            <p class="text-white-50">請填寫以下資料</p>
         </div>
 
         <div class="container d-flex justify-content-center text-white">
             <form action="" method="post">
                 <div class="row">
                     <div class="col mb-3">
-                        <label class="form-label">First Name</label>
-                        <input type="text" class="form-control" name="fName" placeholder="Ohtani">
+                        <label class="form-label">姓</label>
+                        <input type="text" class="form-control" name="fName" placeholder="大谷">
                     </div>
                     <div class="col mb-3">
-                        <label class="form-label">Last Name</label>
-                        <input type="text" class="form-control" name="lName" placeholder="Shohei">
+                        <label class="form-label">名</label>
+                        <input type="text" class="form-control" name="lName" placeholder="翔平">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label">Email</label>
+                        <label class="form-label">電子郵件</label>
                         <input type="email" class="form-control" name="email" placeholder="example@example.com">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label">Nationality</label>&nbsp;
+                        <label class="form-label">國籍</label>&nbsp;
                         <!-- <input type="text" class="form-control" name="nation" placeholder="Japan"> -->
                         <?php include_once 'nationality.html' ?>
                     </div>
                     <div class="form-group mb-3">
-                        <label class="form-label">Gender</label>&nbsp;
+                        <label class="form-label">性別</label>&nbsp;
                         <input type="radio" class="form-check-input" name="gender" id="male" value="male">
-                        <label for="male" class="form-input-label" style="color: #a8caea;">Male</label>&nbsp;
+                        <label for="male" class="form-input-label" style="color: #a8caea;">男性</label>&nbsp;
                         <input type="radio" class="form-check-input" name="gender" id="female" value="female">
-                        <label for="male" class="form-input-label" style="color: #e5c9d7">Female</label>&nbsp;
+                        <label for="male" class="form-input-label" style="color: #e5c9d7">女性</label>&nbsp;
                         <input type="radio" class="form-check-input" name="gender" id="bisex" value="bisex">
-                        <label for="male" class="form-input-label" style="color: #cf9fff;">Bisexual</label>
+                        <label for="male" class="form-input-label" style="color: #cf9fff;">雙性</label>
                     </div>
                 </div>
             </form>
         </div>
         <div class="container d-flex justify-content-center">
-            <button type="submit" class="btn btn-outline-light" >Enter</button>
+            <button type="submit" class="btn btn-outline-light" >確認</button>
         </div>
     </div>
 </body>
